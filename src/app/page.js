58c9660dcd10a14de2a05e7e3282a4b792677c1f
gökhan baysal <53935721+bilinmeyenbirhataolustu@users.noreply.github.com>
2025-05-06 16:36:13 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -8,6 +8,7 @@ export default function Home() {
       <h1 className="text-3xl font-bold ">Hello, Next.js!</h1>
       <div className="box-border size-32 border-4 p-4 text-center bg-inherit">
 ben sigara dumanın altında yana yana en sonunda kül oldum
+    dsfvdsfmngödfgmlgdf
 </div>
     </div>
  
